e90b0135956a363525bedd392c5cb6ab59e7a061
Serge410 <serge410@gmail.com>
2023-09-21 18:51:30 +0800

    Первый Commit

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log(4/0);
+console.log("string" * 5);
+// alert("Hello, World!");
+
+//  let answer = confirm("Вы все еще здесь?")
+//  console.log(answer)
+
+let answer = prompt("Вы все еще здесь?", "Да")
+ console.log(answer)
